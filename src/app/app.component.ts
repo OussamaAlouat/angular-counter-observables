10ec24a031319208a0a668c0a9f7abd6d6d53383
oussamaAlouat <al317257@uji.es>
2020-01-23 13:16:18 +0100

    changed name of some variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,14 @@ import { TimeService } from './time.service';
 })
 export class AppComponent implements OnInit {
   title = 'Secounds counter';
-  public seconds: number;
+  public sec: number;
+
   constructor(private timeService: TimeService){}
   ngOnInit() {
     this.timeService.seconds.subscribe(
       (data:number) => {
-        this.seconds = data;
+        this.sec = data;
+
       }
     )
   }
