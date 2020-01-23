a0cc7e9b9802db648006f1e80cd6c9c40611f853
oussamaAlouat <al317257@uji.es>
2020-01-23 13:07:52 +0100

    Used the new time service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TimeService } from './time.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularObservablesClock';
+export class AppComponent implements OnInit {
+  title = 'Secounds counter';
+  public seconds: number;
+  constructor(private timeService: TimeService){}
+  ngOnInit() {
+    this.timeService.seconds.subscribe(
+      (data:number) => {
+        this.seconds = data;
+      }
+    )
+  }
 }
